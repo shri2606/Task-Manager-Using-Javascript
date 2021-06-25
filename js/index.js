31a8f940a7cd6766098eeb88069a0d444c4345f2
Shridhar Shinde <shindeshridharv@gmail.com>
2021-06-25 12:36:21 +0530

    Add card

---
+++ js/index.js
@@ -0,0 +1,40 @@
+
+const taskContainer = document.querySelector(".task__container");
+
+
+
+
+const saveChanges = () => {
+	const taskData = {
+		id: `${Date.now()}`,  // Unique Number for id 
+		imageUrl: document.getElementById("imageUrl").value,
+		taskTitle: document.getElementById("taskTitle").value,
+		taskType: document.getElementById("tasktype").value,
+		taskDescription: document.getElementById("taskdescription").value,
+	};
+
+
+	const newCard = `<div class="col-md-6 col-lg-4" id=${taskData.id}>
+							<div class="card">
+								<div class="card-header d-flex justify-content-end gap-2">
+									
+									<button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
+									<button type="button" class="btn btn-outline-danger"><i class="fas fa-trash"></i></button>
+								</div>
+								<img src=${taskData.imageUrl} class="card-img-top" alt="...">
+								<div class="card-body">
+									<h5 class="card-title">${taskData.taskTitle}</h5>
+									<p class="card-text">${taskData.taskDescription}</p>
+									<a href="#" class="btn btn-primary">${taskData.taskType}</a>
+								</div>
+								<div class="card-footer">
+									
+									<button type="button" class="btn btn-outline-primary float-end">Open</button>
+								</div>
+							</div>
+						</div>`
+	
+
+	taskContainer.insertAdjacentHTML("beforeend", newCard);
+
+};
